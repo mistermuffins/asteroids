7ffb82fa1c5cdee3c95271658a9ba2e5043395d3
mistermuffins <muffinsmcarthur@gmail.com>
2024-12-13 23:09:26 -0800

    Create and draw a Player.

--- main.py
+++ main.py
@@ -3,6 +3,7 @@
 import pygame
 
 from constants import *
+from player import Player
 
 BLACK = (0, 0, 0)
 
@@ -14,12 +15,18 @@ def main():
   screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
   clock = pygame.time.Clock()
   dt = 0
+  player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
   while True:
     for event in pygame.event.get():
       if event.type == pygame.QUIT:
         return
+
+    # Order of calls is important! Drawing the player before filling the screen
+    # will just end up with a black screen.
     screen.fill(BLACK)
+    player.draw(screen)
+
     pygame.display.flip()
     dt = clock.tick(60) / 1000
 
