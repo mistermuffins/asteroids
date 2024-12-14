02f06c11b8c11766ba97dcb9687c1491f2866563
mistermuffins <muffinsmcarthur@gmail.com>
2024-12-13 22:57:16 -0800

    Setup game clock.

--- main.py
+++ main.py
@@ -12,7 +12,8 @@ def main():
 
   pygame.init()
   screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-  print("Starting asteroids!")
+  clock = pygame.time.Clock()
+  dt = 0
 
   while True:
     for event in pygame.event.get():
@@ -20,6 +21,7 @@ def main():
         return
     screen.fill(BLACK)
     pygame.display.flip()
+    dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
